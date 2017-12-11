225929143dc61341755b3a5e6e0a1bd922cc4860
Jack Henry <jackhenry4@verizon.net>
2017-12-11 14:14:51 -0500

    Version 0.1.2
    +BattleGround class is complete
    +On to testing phase

--- BattleGround.java
+++ BattleGround.java
@@ -46,15 +46,51 @@ public class BattleGround
         public void newWar(){ // war play by play
             
             int Good = 0; // good army
-            int Evil = 0; //evil army
+            int Evil = 0; // evil army
             
             while(Good < troopCount && Evil < troopCount){
             
-                System.out.println("The Eternal Conflict begins anew. Awaken, champions, and fufill your DESTINY!!!!!");
+                System.out.println("The Eternal Conflict begins anew. Awaken champions, and fufill your DESTINY!!!!!");
                 System.out.println("");
                 System.out.println("");
                 
-                //good.get(Good1).takeDamage
+                good.get(Good).takeDamage(evil.get(Evil).damage()); //good army takes damage
+                
+                System.out.println("");
+                
+                System.out.println("Evil " + Evil + " Attack Damage is " + evil.get(Good).damage() + 
+                " towards " + Good + " the heroic creatures! ");
+                
+                System.out.println("");
+                
+                evil.get(Evil).takeDamage(good.get(Good).damage()); // evil army takes damage
+                
+                System.out.println("Good " + Good + " Attack Damage is " + good.get(Evil).damage() + 
+                " towards " + Evil + " the demonic hoard! ");
+                
+                System.out.println("");
+                
+                if(good.get(Good).isAlive() != true){
+                    System.out.println("Good " + Good + " Soldier is slain. Another casuality of the forces of light. ");
+                    Good++;
+                }
+                if(evil.get(Evil).isAlive() != true){
+                    System.out.println("Evil " + Evil + " Soldier is slain. A small dent against the tide of darkness. ");
+                    Evil++;
+                }
+                System.out.println("The battle rages on as new fighters move foward...");
+                System.out.println("");
+                System.out.println("");
             }
-    }
+            System.out.println("The battle draws to a close...");
+            if(Good < Evil){
+                System.out.println("The forces of light have triumphed today, but how long will peace last?");
+            }
+            else if(Evil < Good){
+                System.out.println("Darkness has consmed all hope in this land, but their reign will not last...");
+            }
+            else{
+            System.out.println("The balance holds, at least until the next inevatable conflict...");
+            }
+        }
 }
